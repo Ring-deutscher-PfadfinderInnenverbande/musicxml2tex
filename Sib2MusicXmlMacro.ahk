d8119bc0074bfa6bc10d1fde89767abc13c31e67
manureini <1554111+manureini@users.noreply.github.com>
2022-08-27 19:44:17 +0200

    first simple version

---
+++ Sib2MusicXmlMacro.ahk
@@ -0,0 +1,67 @@
+﻿#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
+; #Warn  ; Enable warnings to assist with detecting common errors.
+
+#SingleInstance force
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+
+
+^F2::
+SetKeyDelay -1 ;
+
+WinActivate,, Sibelius
+CoordMode, Mouse , Window
+
+Loop Files, D:\tmp\ocr2\sib\*.sib
+{
+
+; open
+
+click 20, 50
+Sleep 1000
+
+click 20, 150
+Sleep 1000
+
+Send %A_LoopFileName%
+Send {Enter}
+
+sleep 3000
+
+
+; export
+
+
+Sleep 1000
+
+click 20, 50
+Sleep 1000
+
+click 20, 380
+Sleep 1000
+
+click 350, 300
+Sleep 1000
+
+click 500, 350
+Sleep 1500
+
+Send, {ENTER}
+
+Sleep 5000
+
+; close
+click 20, 50
+Sleep 1000
+
+click 20, 170
+Sleep 1000
+
+click 320, 110
+Sleep 1000
+
+}
+
+ 
+MsgBox done
+
---
+++ musicxml2tex/musicxml2tex/Program.cs
@@ -0,0 +1,101 @@
+﻿
+using MusicXmlSchema;
+using System.Xml;
+using System.Xml.Serialization;
+
+var inputDir = "./input";
+
+
+foreach (var file in Directory.GetFiles(inputDir))
+{
+    var xmlStream = File.OpenRead(file);
+
+    var reader = XmlReader.Create(xmlStream, new XmlReaderSettings() { ConformanceLevel = ConformanceLevel.Document, DtdProcessing = DtdProcessing.Ignore });
+    var obj = new XmlSerializer(typeof(ScorePartwise)).Deserialize(reader) as ScorePartwise;
+
+    var part = obj.Part.First();
+
+
+    var measures = part.Measure;
+
+    var wordPart = string.Empty;
+
+    var lastChord = string.Empty;
+
+    var result = string.Empty;
+
+    foreach (var measure in measures)
+    {
+        if (measure.Print.FirstOrDefault()?.NewSystem == YesNo.Yes)
+        {
+            result += "\n";
+        }
+
+        var chord = GetHarmony(measure);
+
+        if (chord != null && lastChord != chord)
+        {
+            result += $"\\[{chord}]";
+            lastChord = chord;
+        }
+
+        foreach (var note in measure.Note)
+        {
+            foreach (var lyric in note.Lyric)
+            {
+                if (lyric.Syllabic == Syllabic.Single)
+                {
+                    result += lyric.Text.Value + " ";
+                    continue;
+                }
+
+                wordPart += lyric.Text.Value;
+
+                if (lyric.Syllabic == Syllabic.End)
+                {
+                    result += wordPart + " ";
+                    wordPart = string.Empty;
+                }
+            }
+        }
+    }
+
+    Console.WriteLine(result);
+}
+
+static string GetHarmony(ScorePartwisePartMeasure? measure)
+{
+    var harmony = measure.Harmony.FirstOrDefault();
+
+    if (harmony == null)
+        return null;
+
+    var root = harmony.Root.First();
+
+    var chord = root.RootStep.Value;
+
+    var chordStr = chord.ToString();
+
+    if (harmony.Kind.Count > 1)
+        throw new NotSupportedException();
+
+    var kind = harmony.Kind.First();
+
+    if (kind.Value == KindValue.Major)
+    {
+    }
+    else if (kind.Value == KindValue.Minor)
+    {
+        chordStr += "m";
+    }
+    else if (kind.Value == KindValue.Dominant)
+    {
+        chordStr += "7";
+    }
+    else
+    {
+        throw new NotSupportedException();
+    }
+
+    return chordStr;
+}
